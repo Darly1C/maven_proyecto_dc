6803cb8d87333fdc3df7e5d68b94660cd881678a
Darly Castillo <dscastillop@uce.edu.ec>
2021-09-08 19:29:21 -0500

    ejercicios estaticos

---
+++ src/main/java1/ejercicios/nonacces/staticos/Paciente.java
@@ -0,0 +1,40 @@
+package ejercicios.nonacces.staticos;
+
+public class Paciente {
+	// los atributos estáticos no hace falta SET y GET
+	private String nombre;
+	private int edad;
+	private static String nacionalidad = "ECUATORIANO";
+	private static int edadLimite = 65;
+
+	public void registrar(String nombre) {
+		System.out.println("Se registra el paciente: " + nombre);
+
+	}
+
+	private static void consultarNacionalidad() {
+		System.out.println("");
+	}
+
+	public static void consultarNacionalidadStatica() {
+		System.out.println("La nacionalidad de:  " + nacionalidad);
+	}
+
+	// Métodos SET y GET
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+}
