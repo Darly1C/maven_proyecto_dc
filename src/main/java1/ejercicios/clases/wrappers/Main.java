8583b49301c7f2edfb84bef7f139bc6c6d68d305
Darly Castillo <dscastillop@uce.edu.ec>
2021-09-15 19:51:53 -0500

    Clases Wrapper

---
+++ src/main/java1/ejercicios/clases/wrappers/Main.java
@@ -0,0 +1,42 @@
+package ejercicios.clases.wrappers;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// byte 
+		// primitivo
+		byte datoByte = 2;
+		// Wrapper
+		Byte datoByteWrapper = 3;
+		System.out.println("primitivo: " + datoByte);
+		System.out.println("Wrapper: " + datoByteWrapper);
+		
+		// short 
+		//primitivo
+		short datoShort = 1;
+		// Wrapper
+		Short datoShortWrapper = 1;
+		System.out.println("primitivo: " + datoShort);
+		System.out.println("Wrapper: " + datoShortWrapper);
+		
+		// int 
+		//primitivo
+		int edad =45;
+		// Wrapper
+		Integer edadWrapper = 48;
+		System.out.println("primitivo: " + edad);
+		System.out.println("Wrapper: " + edadWrapper);
+		
+		
+		//long
+		//primitivo
+		long valorGrande = 12345L;
+		// Wrapper
+		Long valorGrandeWrapper = 123654L;
+		System.out.println("primitivo: " + valorGrande);
+		System.out.println("Wrapper: " + valorGrandeWrapper);
+	}
+
+}
