cfaf1c6b24a6866f46460026807e1703c00f59cb
Darly Castillo <dscastillop@uce.edu.ec>
2021-09-20 20:06:16 -0500

    Clases

--- src/main/java1/ejercicios/fechas/CursoVacacional.java
+++ src/main/java1/ejercicios/fechas/CursoVacacional.java
@@ -3,29 +3,42 @@ package ejercicios.fechas;
 import java.time.LocalDate;
 
 public class CursoVacacional {
-	
+
 	private String nombre;
 	private LocalDate fechaInicio;
 	private LocalDate fechaFin;
-	
+	private Instructor instructor;
+
 	// Métodos SET Y GET
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public LocalDate getFechaInicio() {
 		return fechaInicio;
 	}
+
 	public void setFechaInicio(LocalDate fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
+
 	public LocalDate getFechaFin() {
 		return fechaFin;
 	}
+
 	public void setFechaFin(LocalDate fechaFin) {
 		this.fechaFin = fechaFin;
 	}
 
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
 }
